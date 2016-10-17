a213b2268012dfd5d83fa49245db187f29a9b7bf
captaincode0 <developerdiego0@gmail.com>
2016-10-17 14:10:47 -0500

    New minimum viable product

---
+++ libs/app/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$(".btn-collapse").sideNav({
+		menuWidth: 300,
+		edge: "left",
+		closeOnClick: true
+	});
+
+  	$('.carousel.carousel-slider').carousel({full_width: true});        
+});
